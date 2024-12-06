fcf644c4652a1bd64788dfff026611804a1a5b7c
Rishang <rishangkumar9693@gmail.com>
2024-12-06 19:24:14 +0530

    adding signup func

--- src/Ui/SignUp.tsx
+++ src/Ui/SignUp.tsx
@@ -1,10 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "../components/ui/button";
 import { Input } from "../components/ui/input";
 import { Label } from "@radix-ui/react-label";
 import { Link } from "react-router-dom";
 
+type inputtypes = {
+  username: string;
+  email: string;
+  password: string;
+  avatar: File | null;
+  coverimg?: File | null;
+};
+
+const initialvalues = {
+  username: "",
+  email: "",
+  password: "",
+  avatar: null,
+  coverimg: null,
+};
+
 const SignUp = () => {
+  const [formvalues, setformValues] = useState<inputtypes>(initialvalues);
+
+  const handlechange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value, files } = e.target;
+
+    if (name === "avatar" || name === "coverimg") {
+      setformValues((prev) => ({
+        ...prev,
+        [name]: files ? files[0] : null,
+      }));
+    } else {
+      setformValues((prev) => ({
+        ...prev,
+        [name]: value,
+      }));
+    }
+  };
+   
+
+ const handleSignUpSubmit =(e: React.FormEvent<HTMLFormElement>)=>{
+   e.preventDefault();
+ }
+
   return (
     <>
       <div className="mx-auto max-w-screen-xl flex items-center justify-center   px-4 py-16 sm:px-6 lg:px-8">
@@ -15,15 +54,18 @@ const SignUp = () => {
             </h1>
           </div>
 
-          <form action="#" className="mx-auto mb-0 mt-8 max-w-md space-y-4">
+          <form onSubmit={handleSignUpSubmit} className="mx-auto mb-0 mt-8 max-w-md space-y-4">
             <div>
-              <label htmlFor="email" className="sr-only">
+              <label htmlFor="username" className="sr-only">
                 UserName
               </label>
 
               <div className="relative">
                 <Input
-                  type="email"
+                  type="text"
+                  name="username"
+                  value={formvalues.username}
+                  onChange={handlechange}
                   className="w-full rounded-lg text-black border-gray-200 p-4 pe-12 text-sm shadow-sm"
                   placeholder="Enter UserName"
                 />
@@ -37,6 +79,9 @@ const SignUp = () => {
               <div className="relative">
                 <Input
                   type="email"
+                  name="email"
+                  value={formvalues.email}
+                  onChange={handlechange}
                   className="w-full rounded-lg text-black border-gray-200 p-4 pe-12 text-sm shadow-sm"
                   placeholder="Enter email"
                 />
@@ -49,7 +94,10 @@ const SignUp = () => {
 
               <div className="relative">
                 <Input
-                  type="email"
+                  type="password"
+                  name="password"
+                  value={formvalues.password}
+                  onChange={handlechange}
                   className="w-full rounded-lg text-black border-gray-200 p-4 pe-12 text-sm shadow-sm"
                   placeholder="Enter Password"
                 />
@@ -61,20 +109,32 @@ const SignUp = () => {
                 <Label htmlFor="picture" className="">
                   Avatar
                 </Label>
-                <Input type="file" placeholder="Avatar" />
+                <Input
+                  type="file"
+                  name="avatar"
+                  accept="image/*"
+                  placeholder="Avatar"
+                  onChange={handlechange}
+                />
               </div>
             </div>
             <div>
               <div className="">
                 <Label htmlFor="picture">CoverImage</Label>
-                <Input type="file" placeholder="Cover Img" />
+                <Input
+                  type="file"
+                  placeholder="Cover Img"
+                  accept="image/*"
+                  onChange={handlechange}
+                  name="coverimg"
+                />
               </div>
             </div>
 
             <div className="flex items-center justify-between">
               <Link to={"/login"}>
                 <Button className="text-sm text-paraText" variant={"link"}>
-                I have an account.
+                  I have an account.
                 </Button>
               </Link>
               <Button
